cffe0d8e339680f900fd554f2a805c2ac9df0dd6
Govinda Verma <smartgovindaverma@gmail.com>
2023-04-15 14:29:19 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Hi, My Name is</p>
+      <span>Govinda Verma</span>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Software Developer
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         Know more about me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
