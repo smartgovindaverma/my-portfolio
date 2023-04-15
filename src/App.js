1921bb20f5a15a7edd4c18f10cd2fa696c00e4ad
Govinda Verma <smartgovindaverma@gmail.com>
2023-04-16 01:43:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Know more about me
+         About me
         </a>
       </header>
     </div>
